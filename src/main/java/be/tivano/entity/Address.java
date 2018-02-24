61a11882f27809e6e426403673ee5706c3937108
jeff <jeff.mauro@gmail.com>
2018-02-24 11:07:49 +0100

    add address

---
+++ src/main/java/be/tivano/entity/Address.java
@@ -0,0 +1,22 @@
+package be.tivano.entity;
+
+public class Address {
+    private Long id;
+    private String country;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
