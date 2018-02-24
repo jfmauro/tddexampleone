56e59b1a1431a898350d0afcd908400ff42e6efe
jeff <jeff.mauro@gmail.com>
2018-02-24 14:50:22 +0100

    add Logger

---
+++ src/main/java/be/tivano/entity/Provider.java
@@ -0,0 +1,22 @@
+package be.tivano.entity;
+
+public class Provider {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
